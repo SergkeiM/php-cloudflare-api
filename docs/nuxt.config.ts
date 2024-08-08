304b5437b2c87961dc18bb44cca09f037132305c
SergkeiM <sergio11of@gmail.com>
2024-08-08 23:14:34 +0300

    disabled ssr

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,11 +1,12 @@
 export default defineNuxtConfig({
-  // https://github.com/nuxt-themes/docus
-  extends: ['@nuxt-themes/docus'],
-  devtools: { enabled: true },
+    ssr: false,
+    // https://github.com/nuxt-themes/docus
+    extends: ['@nuxt-themes/docus'],
+    devtools: { enabled: true },
 
-  modules: [
-    // Remove it if you don't use Plausible analytics
-    // https://github.com/nuxt-modules/plausible
-    '@nuxtjs/plausible'
-  ]
+    modules: [
+        // Remove it if you don't use Plausible analytics
+        // https://github.com/nuxt-modules/plausible
+        '@nuxtjs/plausible'
+    ]
 })
