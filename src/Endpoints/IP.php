94b81de13a50a679e5566bc2499e3125df97a14c
SergkeiM <sergio11of@gmail.com>
2024-08-13 17:00:14 +0300

    Pint

--- src/Endpoints/IP.php
+++ src/Endpoints/IP.php
@@ -11,7 +11,7 @@ class IP extends AbstractEndpoint
 {
     /**
      * Get IPs used on the Cloudflare/JD Cloud network
-     * For Cloudflare IPs: https://www.cloudflare.com/ips 
+     * For Cloudflare IPs: https://www.cloudflare.com/ips
      * For JD Cloud IPs: https://developers.cloudflare.com/china-network/reference/infrastructure/ .
      * @link https://developers.cloudflare.com/api/operations/cloudflare-i-ps-cloudflare-ip-details
      *
