406b6e6f871d5f63c8d71283a975bdc52bee2a53
SergkeiM <sergio11of@gmail.com>
2024-08-13 16:59:43 +0300

    added CloudFlare IPs

--- local-test/src/index.php
+++ local-test/src/index.php
@@ -4,24 +4,12 @@ require __DIR__.'/../vendor/autoload.php';
 
 use Dotenv\Dotenv;
 use SergkeiM\CloudFlare\Client;
-use SergkeiM\CloudFlare\Exceptions\RequestException;
 
 $dotenv = Dotenv::createUnsafeMutable(__DIR__.'/../');
 $dotenv->load();
 
 $client = new Client(getenv('CLOUDFLARE_TOKEN'));
 
-$response = $client->zones()->dns()->export(getenv('ZONE_ID'));
+$response = $client->ips()->get();
 
-file_put_contents('./test.txt', $response->body());
-
-$file = file_get_contents('./test.txt', true);
-
-try {
-
-    $response = $client->zones()->dns()->import(getenv('ZONE_ID'), $file);
-
-} catch (RequestException $th) {
-    
-    var_dump($th->getResponse()->toArray());
-}
+file_put_contents('./test.json', json_encode($response->toArray(), JSON_PRETTY_PRINT));
---
+++ src/Endpoints/IP.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace SergkeiM\CloudFlare\Endpoints;
+
+use SergkeiM\CloudFlare\Contracts\CloudFlareResponse;
+
+/**
+ * @link https://developers.cloudflare.com/api/operations/cloudflare-i-ps-cloudflare-ip-details
+ */
+class IP extends AbstractEndpoint
+{
+    /**
+     * Get IPs used on the Cloudflare/JD Cloud network
+     * For Cloudflare IPs: https://www.cloudflare.com/ips 
+     * For JD Cloud IPs: https://developers.cloudflare.com/china-network/reference/infrastructure/ .
+     * @link https://developers.cloudflare.com/api/operations/cloudflare-i-ps-cloudflare-ip-details
+     *
+     * @return CloudFlareResponse Cloudflare/JD Cloud IP Details response
+     */
+    public function get(): CloudFlareResponse
+    {
+
+        return $this->sendGet('/ips');
+    }
+}
